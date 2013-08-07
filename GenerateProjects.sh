167b211d11c4ce1c749a8a355dc9c1c2f942dc2d
Kobra <kobra@pc>
2013-08-07 18:40:41 +0100

    Added clean scripts

--- GenerateProjects.sh
+++ GenerateProjects.sh
@@ -2,5 +2,5 @@
 
 #make sure libqt4-devel or libqt4-dev libqt4-dev-bin are in stalled
 
-./ThirdParty/Premake4.elf --qt-shared --file=Empty.lua clean
+./Clean.sh
 ./ThirdParty/Premake4.elf --qt-shared --file=Empty.lua gmake
