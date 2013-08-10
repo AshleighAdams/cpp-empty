ecae6554aab928145bf0aba4e8c9072f443d4990
Kobra <kobra@pc>
2013-08-10 18:17:42 +0100

    re-added qt-shared after merge from master to remove leftover from this branch

--- GenerateProjects.sh
+++ GenerateProjects.sh
@@ -3,4 +3,4 @@
 #make sure libqt4-devel or libqt4-dev libqt4-dev-bin are in stalled
 
 ./Clean.sh
-./ThirdParty/Premake4.elf --file=Empty.lua gmake
+./ThirdParty/Premake4.elf --qt-shared --file=Empty.lua gmake
